589784d6df701fa969e1dfe3fbb308c22f76dbc6
Qing Li <lqingpku@gmail.com>
2020-01-12 20:51:30 -0700

    Clean up

--- set_env_mpaso_pgi.sh
+++ set_env_mpaso_pgi.sh
@@ -2,13 +2,15 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ga
 
 export PGI=${HOME}/local/pgi/linux86-64/2019
 export CUDA=${HOME}/local/pgi/linux86-64/2019/cuda/10.1
-export NETCDF=${HOME}/local/env-mpas-pgi
-export PNETCDF=${HOME}/local/env-mpas-pgi
-export PIO=${HOME}/local/env-mpas-pgi
-export FFTW=${HOME}/local/env-mpas-pgi
+export OPENMPI=${PGI}/mpi/openmpi-3.1.3
+export ENV_MPAS_PGI=${HOME}/local/env-mpas-pgi
+export NETCDF=${ENV_MPAS_PGI}
+export PNETCDF=${ENV_MPAS_PGI}
+export PIO=${ENV_MPAS_PGI}
+export FFTW=${ENV_MPAS_PGI}
 
-export PATH=$PGI/mpi/openmpi-3.1.3/bin:${PGI}/bin:${PATH}
-export LD_LIBRARY_PATH=${PGI}/lib:${CUDA}/lib64:${LD_LIBRARY_PATH}
+export PATH=${ENV_MPAS_PGI}/bin:${OPENMPI}/bin:${PGI}/bin:${PATH}
+export LD_LIBRARY_PATH=${ENV_MPAS_PGI}/lib:${OPENMPI}/lib:${PGI}/lib:${CUDA}/lib64:${LD_LIBRARY_PATH}
 
 export CORE=ocean
 export AUTOCLEAN=true
