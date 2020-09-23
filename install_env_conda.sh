f1444a93509be72ad9e79b8e5c49c0a1aae0891c
Qing Li <lqingpku@gmail.com>
2020-09-23 13:39:15 -0600

    Update install script for conda

--- install_env_conda.sh
+++ install_env_conda.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 export PNETCDF_VERSION=1.12.0
 export PIO_VERSION=1.10.1
 
@@ -8,10 +10,67 @@ export CONDA_PATH=${HOME}/miniconda3
 
 source ${CONDA_PATH}/etc/profile.d/conda.sh
 
-conda create -y -n mpas -c conda-forge -c xylar python=3.7 \
-geometric_features=0.1.3 mpas_tools=0.0.3 jigsaw=0.9.11 jigsawpy=0.0.2 \
-metis pyflann scikit-image basemap pyamg ffmpeg netcdf-fortran mpich \
-fortran-compiler cxx-compiler c-compiler m4 git cmake pyremap fftw
+function install_mpas_env() {
+    conda create -y -n mpas -c conda-forge -c xylar python=3.7 \
+    geometric_features=0.1.4 mpas_tools=0.0.6 jigsaw=0.9.12 jigsawpy \
+    metis pyflann scikit-image basemap pyamg ffmpeg netcdf-fortran mpich \
+    fortran-compiler cxx-compiler c-compiler m4 git cmake pyremap fftw
+}
+
+function install_pnetcdf() {
+    # install pnetcdf
+    rm -rf pnetcdf-${PNETCDF_VERSION}*
+
+    wget https://parallel-netcdf.github.io/Release/pnetcdf-${PNETCDF_VERSION}.tar.gz
+
+    tar xvf pnetcdf-${PNETCDF_VERSION}.tar.gz
+    cd pnetcdf-${PNETCDF_VERSION}
+
+    ./configure --prefix=${PREFIX}
+    make
+    make install
+
+    cd ..
+}
+
+function install_pio() {
+    # install pio
+    rm -rf ParallelIO pio-${PIO_VERSION}
+
+    git clone git@github.com:NCAR/ParallelIO.git
+    cd ParallelIO
+    git checkout pio$PIO_VERSION
+
+    cd pio
+
+    export PIOSRC=`pwd`
+    git clone https://github.com/PARALLELIO/genf90.git bin
+    cd bin
+    git checkout genf90_140121
+    cd ..
+    git clone https://github.com/CESM-Development/CMake_Fortran_utils.git cmake
+    cd ../..
+
+    export NETCDF=$PREFIX
+    export PNETCDF=$PREFIX
+    export PHDF5=$PREFIX
+    export MPIROOT=$PREFIX
+
+    export FC=mpif90
+    export CC=mpicc
+    mkdir pio-${PIO_VERSION}
+    cd pio-${PIO_VERSION}
+    cmake -D NETCDF_C_DIR=${PREFIX} -D NETCDF_Fortran_DIR=${PREFIX} \
+    -D PNETCDF_DIR=${PREFIX} -D CMAKE_VERBOSE_MAKEFILE=1 \
+    -D CMAKE_INSTALL_PREFIX=${PREFIX} $PIOSRC
+    make
+    cp *.h *.mod ${PREFIX}/include
+    cp *.a ${PREFIX}/lib
+
+    cd ..
+}
+
+# install_mpas_env
 
 conda activate mpas
 
@@ -24,48 +83,6 @@ export MPIF77=mpif77
 export MPIF90=mpif90
 export LDFLAGS="-L${PREFIX}/lib"
 
-# install pnetcdf
-rm -rf pnetcdf-${PNETCDF_VERSION}*
+# install_pnetcdf
 
-wget https://parallel-netcdf.github.io/Release/pnetcdf-${PNETCDF_VERSION}.tar.gz
-
-tar xvf pnetcdf-${PNETCDF_VERSION}.tar.gz
-cd pnetcdf-${PNETCDF_VERSION}
-
-./configure --prefix=${PREFIX}
-make
-make install
-
-cd ..
-
-# install pio
-rm -rf ParallelIO pio-${PIO_VERSION}
-
-git clone git@github.com:NCAR/ParallelIO.git
-cd ParallelIO
-git checkout pio$PIO_VERSION
-
-cd pio
-
-export PIOSRC=`pwd`
-git clone https://github.com/PARALLELIO/genf90.git bin
-git clone https://github.com/CESM-Development/CMake_Fortran_utils.git cmake
-cd ../..
-
-export NETCDF=$PREFIX
-export PNETCDF=$PREFIX
-export PHDF5=$PREFIX
-export MPIROOT=$PREFIX
-
-export FC=mpif90
-export CC=mpicc
-mkdir pio-${PIO_VERSION}
-cd pio-${PIO_VERSION}
-cmake -D NETCDF_C_DIR=${PREFIX} -D NETCDF_Fortran_DIR=${PREFIX} \
--D PNETCDF_DIR=${PREFIX} -D CMAKE_VERBOSE_MAKEFILE=1 \
--D CMAKE_INSTALL_PREFIX=${PREFIX} $PIOSRC
-make
-cp *.h *.mod ${PREFIX}/include
-cp *.a ${PREFIX}/lib
-
-cd ..
+install_pio
