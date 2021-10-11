25e12fce2c3204e7c53fa101f97dabcbdba0c229
Qing Li <lqingpku@gmail.com>
2021-10-11 21:38:12 +0800

    Update tianhe env

--- set_env_tianhe2.sh
+++ set_env_tianhe2.sh
@@ -1,27 +1,37 @@
-# module purge
-
+env=$1
 # load modules
-module load intelcompiler/14.0.2
-module load MPI/mpich/3.2.1-icc-14.0.2-dynamic
-module load netcdf/4.5.0-icc-14.0.2
-module load pnetcdf/1.9.0-icc-14.0.2
-module load PIO/2.4.0-icc-14.0.2
-module load cmake/3.20.2
+module purge
+if [[ ${env} == "intel" ]]; then
+    echo "Loading environment for Intel"
+    module load intelcompiler/14.0.2
+    module load MPI/mpich/3.2.1-icc-14.0.2-dynamic
+    module load netcdf/4.5.0-icc-14.0.2
+    module load pnetcdf/1.9.0-icc-14.0.2
+    module load PIO/2.4.0-icc-14.0.2
+    module load cmake/3.20.2
+    export PIO="/BIGDATA1/app/PIO/2.4.0-icc-14.0.2"
+    export USE_PIO2=true
+elif [[ ${env} == "gnu" ]]; then
+    echo "Loading environment for GNU"
+    module load gcc/5.2.0
+    module load MPI/mpich/3.2.1-gcc-5.2.0-dynamic
+    module load netcdf/4.6.3-gcc-5.2.0
+    module load pnetcdf/1.11.0-gcc-5.2.0
+    module load PIO/1.9.23-gcc-5.2.0
+    module load cmake/3.17.0-gcc-5.2.0
+    export PIO="/BIGDATA1/app/PIO/1.9.23-gcc-5.2.0"
+    export USE_PIO2=false
+fi
 
-echo "NETCDF is set to ${NETCDF}"
 export NETCDFF=${NETCDF}
+echo "NETCDF is set to ${NETCDF}"
 echo "NETCDFF is set to ${NETCDFF}"
 echo "PNETCDF is set to ${PNETCDF}"
-export PIO="/BIGDATA1/app/PIO/2.4.0-icc-14.0.2"
 echo "PIO is set to ${PIO}"
-
-# export FFTW=/lustre/scratch3/turquoise/qingli/software/fftw3/3.3.8/gcc-6.4.0
-# echo "FFTW is set to ${FFTW}"
 
 # flags
 export CORE=ocean
 export AUTOCLEAN=true
-export USE_PIO2=true
 
 export LANG=en_US.utf8
 export LC_ALL=en_US.utf8
